8df946b87320d00e9f171420e07a0217281db817
HopoZ <2166632799@qq.com>
2025-06-29 15:46:38 +0800

    update

--- src/main2.py
+++ src/main2.py
@@ -19,3 +19,53 @@ x,y,z = test()
 #函数作为参数传递
 
 #EXP 匿名函数 lambda 参数:处理 函数体只能写一行代码
+
+print("------------\n")
+#EXP 类继承
+class Animal:
+    def sound(self):
+        print("动物的声音")
+
+class Dog(Animal):
+    def sound(self):
+        print("汪汪")
+
+a = Dog()
+a.sound() #汪汪
+print("------------\n")
+#EXP 生成器（Generator）
+def count_up_to(n):
+    count = 1
+    while count <= n:
+        yield count  # 暂停并返回当前值
+        count += 1
+# 使用for循环迭代生成器
+for num in count_up_to(3):
+    print(num)
+print("------------\n")
+#EXP 装饰器（Decorator）
+def decorator(func):
+    def wrapper():
+        print("装饰器执行")
+        func()
+    return wrapper
+
+@decorator
+def my_function():
+    print("函数执行")
+
+my_function()
+print("------------\n")
+#EXP pyhon单例
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+s1 = Singleton()
+s2 = Singleton()
+print(s1 is s2) # true
+print("------------\n")
